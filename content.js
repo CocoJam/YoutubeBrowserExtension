c6dab819f8e18db3fb12fd82f7f7c9eb0c40e7d6
razormixer <razormixer@gitlab.nectar.auckland.ac.nz>
2018-04-24 10:57:47 +1200

    window listen on message between content and html
    The window listen will show different message source to detect difference source to all communcation between content and html. window on message from content to html is window.source === window, while html to content is window.source !==window.

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 //detecting what browser it is, if it is chrome it will return chrome. Otherwise then it will return associated browser.
 const browser = chrome || browers;
 window.addEventListener("message", function (event) {
+    //communication from html to content goes here
+    if (event.source !== window && event.origin === "https://www.youtube.com") {
 
-}
+    }
+});
