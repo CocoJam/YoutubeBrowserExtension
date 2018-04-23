724e633b30908aa87fb945fd2468bed7e07e28b7
razormixer <razormixer@gitlab.nectar.auckland.ac.nz>
2018-04-24 11:04:31 +1200

    content script received youtube API message
    Upon receiving youtube Iframe API message event the origin is detected and parse into json for later usage

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 //detecting what browser it is, if it is chrome it will return chrome. Otherwise then it will return associated browser.
 const browser = chrome || browers;
 window.addEventListener("message", function (event) {
-    //communication from html to content goes here
+    console.log(event);
+    //communication from html to content goes here and detecting youtube messages from youtube iframe API
     if (event.source !== window && event.origin === "https://www.youtube.com") {
-
+        var json = JSON.parse(event.data);
+        console.log(json);
     }
 });
