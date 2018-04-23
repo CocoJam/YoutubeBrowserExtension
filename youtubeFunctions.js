2ba58c15ad8d5d519221bf02de3e877ac5f27a86
razormixer <razormixer@gitlab.nectar.auckland.ac.nz>
2018-04-23 20:19:25 +1200

    YoutubeFunction.js implemented youtube iframe functions
    This implementation consist of youtube iframe API's function, such that it allow iframes to be generated when youtube Iframe API has finished loaded.

---
+++ youtubeFunctions.js
@@ -0,0 +1,37 @@
+//variables that will be used to reference
+var player;
+var currentID = "";
+var currentTime = 0;
+//Method called once the iframe API finish loading.
+//referencing the example codes from https://developers.google.com/youtube/iframe_api_reference
+function onYouTubeIframeAPIReady() {
+    //The videoId is the id of the video wanted
+    player = new YT.Player('player_youtube', {
+        height: '390',
+        width: '640',
+        videoId: currentID,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+    iframe = this.player.a;
+    console.log("INIT TAB")
+}
+
+//event emits when video is ready to be play after loaded
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+//detect youtube iframe video state change.
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        // setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
