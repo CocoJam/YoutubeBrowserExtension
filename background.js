8acb9b8897462f2045038f071da7816d3e13e290
razormixer <razormixer@gitlab.nectar.auckland.ac.nz>
2018-04-24 20:13:47 +1200

    Added Browser_action plus handler
    The browser action is handled by sending a message to specific tab's content script.

--- background.js
+++ background.js
@@ -8,3 +8,11 @@ var portFromCS = null;
 function connected(port) {
     portFromCS = port;
 }
+//This handles the clicking actions from the browser as browser actions
+browser.browserAction.onClicked.addListener(handleClick);
+
+//Sense to button click of the window, then sending a message down to specific tab's content script.
+function handleClick(tab) {
+    console.log(tab);
+    browser.tabs.sendMessage(tab.id, {});
+}
